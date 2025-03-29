c110161685a54b8eab6472d28e99a40410683322
garrido8 <adriangarrido15.9@gmail.com>
2025-03-29 10:56:06 +0100

    Gemini insertado en la aplicación

--- src/app/services/gemini.service.ts
+++ src/app/services/gemini.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { GoogleGenAI } from '@google/genai';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeminiService {
+  private ai: GoogleGenAI;
 
-  constructor() { }
+  constructor() {
+    this.ai = new GoogleGenAI({ apiKey: 'AIzaSyAv950D51AxiOGHZMCmghSLzV074ECOsPQ' });
+  }
+
+  async generateContent(prompt: string): Promise<string> {
+    try {
+      const response = await this.ai.models.generateContent({
+        model: 'gemini-2.0-flash',
+        contents: prompt,
+      });
+      return response.text || 'No response received';
+    } catch (error) {
+      console.error('Error generating content:', error);
+      return 'Error occurred';
+    }
+  }
 }
--- src/app/vet/components/chatbot/chatbot.component.ts
+++ src/app/vet/components/chatbot/chatbot.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { GeminiService } from '../../../services/gemini.service';
 
 @Component({
   selector: 'app-chatbot',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './chatbot.component.html',
   styleUrl: './chatbot.component.css'
 })
-export class ChatbotComponent {
+export class ChatbotComponent implements OnInit{
 
+  private gemini = inject( GeminiService )
+
+  public responseText: string = ''
+
+  async ngOnInit() {
+    this.responseText = await this.gemini.generateContent('Explain how AI works in a few words');
+    console.log(this.responseText);
+  }
 }
