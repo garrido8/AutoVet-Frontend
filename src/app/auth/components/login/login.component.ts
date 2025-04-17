043808589f5bfbcd334b5a8e26dc32907fdef485
garrido8 <adriangarrido15.9@gmail.com>
2025-04-17 16:50:18 +0200

    Añadido componente de login con validación de formulario y estilos

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthRoutingModule } from '../../auth-routing.module';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
 export class LoginComponent {
+
+  private fb = inject(FormBuilder)
+
+  public form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  public logIn(): void {
+    if (this.form.valid) {
+      console.log('Email:', this.form.value.email);
+      console.log('Password:', this.form.value.password);
+    } else {
+      console.log('Formulario inválido 😬');
+      this.form.markAllAsTouched(); // Para mostrar errores si los hay
+    }
+  }
 
 }
