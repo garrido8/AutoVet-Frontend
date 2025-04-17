e376a2f98b99379dc1715e0d4dcc16ebb39136d1
garrido8 <adriangarrido15.9@gmail.com>
2025-04-17 10:17:33 +0200

    Añadido prompt para adivinar la raza del animal

--- src/environments/prompt-settings.ts
+++ src/environments/prompt-settings.ts
@@ -8,3 +8,5 @@
   export const expertPrompt: string = 'Eres un experto en veterinaria que sabes todo sobre animales y eres capaz de dar consejos sobre animales de manera profesional'
 
   export const shortResponse: string = 'Dame una respuesta corta con la información esencial'
+
+  export const guessBreed: string = 'Podrías decirme la raza del animal que aparece en la imagen. Solo la raza, no quiero más palabras, simplemente el nombre de la raza'
