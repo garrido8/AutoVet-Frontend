a15d8a058753a414a9afbad9d5518e64a16322e8
garrido8 <adriangarrido15.9@gmail.com>
2025-04-20 20:33:30 +0200

    Creada página de ver las incidencias de las mascotas del usuario

--- src/app/interfaces/appoinment.interface.ts
+++ src/app/interfaces/appoinment.interface.ts
@@ -2,13 +2,14 @@ import { Staff } from "./staff.interface";
 
 export interface Appoinment {
   pk?:                  number;
-  mascota:             number;
-  titulo:              string;
-  descripcion:         string;
-  fecha_creacion:      Date;
-  fecha_resolucion:    Date;
-  estado:              string;
-  urgencia:            boolean;
-  archivo_adjuntado:   null;
-  trabajador_asignado: Staff;
+  mascota:              number;
+  petName?:             string;
+  titulo:               string;
+  descripcion:          string;
+  fecha_creacion:       Date;
+  fecha_resolucion:     Date;
+  estado:               string;
+  urgencia:             boolean;
+  archivo_adjuntado:    null;
+  trabajador_asignado:  Staff;
 }
--- src/app/user/components/appoinments/appoinments.component.ts
+++ src/app/user/components/appoinments/appoinments.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserInfoService } from '../../../services/user-info.service';
+import { AuthService } from '../../../services/auth.service';
+import { PetService } from '../../../services/pet.service';
+import { AppoinmentService } from '../../../services/appoinment.service';
+import { Appoinment } from '../../../interfaces/appoinment.interface';
+import { Client } from '../../../interfaces/client.interface';
 
 @Component({
   selector: 'app-appoinments',
@@ -6,6 +12,44 @@ import { Component } from '@angular/core';
   templateUrl: './appoinments.component.html',
   styleUrl: './appoinments.component.css'
 })
-export class AppoinmentsComponent {
+export class AppoinmentsComponent implements OnInit {
+
+  private userInfoService = inject( UserInfoService )
+  private authService = inject( AuthService )
+  private petService = inject( PetService )
+  private appoinmentService = inject( AppoinmentService )
+
+  public appoinments: Appoinment[] = []
+
+  ngOnInit(): void {
+    const token = this.userInfoService.getToken();
+
+    if (token) {
+      this.authService.getUserPerEmail(token)
+        .subscribe(response => {
+          const user: Client = response[0];
+
+          this.petService.getPetByOwner(user.id!)
+            .subscribe(pets => {
+
+              pets.forEach(pet => {
+                this.appoinmentService.getAppoinmentByPet(pet.pk!)
+                  .subscribe(appoinments => {
+                    // Añadir el nombre de la mascota a cada cita
+                    const enrichedAppoinments = appoinments.map(app => ({
+                      ...app,
+                      petName: pet.nombre
+                    }));
+
+                    // Agregar al array general
+                    this.appoinments = [ ...this.appoinments, ...enrichedAppoinments ];
+                  });
+              });
+
+            });
+        });
+    }
+  }
+
 
 }
