99f2bf668ae6fe0c1c1c5e93e837fdd22950cb09
garrido8 <adriangarrido15.9@gmail.com>
2025-05-01 12:34:07 +0200

    El trabajador ya puede ver sus clientes

--- src/app/staff/components/clients/clients.component.ts
+++ src/app/staff/components/clients/clients.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../../services/auth.service';
+import { UserInfoService } from '../../../services/user-info.service';
+import { Client } from '../../../interfaces/client.interface';
+import { Staff } from '../../../interfaces/staff.interface';
 
 @Component({
   selector: 'app-clients',
@@ -6,6 +11,43 @@ import { Component } from '@angular/core';
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit, OnDestroy{
+
+  private subscriptions = new Subscription();
+  private userInfoService = inject( UserInfoService)
+  private authService = inject( AuthService )
+
+  public clients: Client[] = [];
+
+  ngOnInit(): void {
+    const token = this.userInfoService.getToken()
+
+    if( token ) {
+      this.authService.getStaffPerEmail(token)
+      .subscribe( response => {
+        if ( response.length > 0 ) {
+          const staffMember: Staff = response[0];
+          const clientsIds = staffMember.assigned_clients;
+
+          clientsIds.forEach( (clientId: number) => {
+            this.authService.getUserPerId(clientId)
+            .subscribe( response => {
+              if ( response ) {
+                this.clients.push(response);
+              } else {
+                console.log('No client found with this ID.');
+              }
+            })
+          } )
+          console.log('Client Data Received:', this.clients);
+        } else {
+          console.log('No client found with this email.');
+        }
+      })
+    }
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }
