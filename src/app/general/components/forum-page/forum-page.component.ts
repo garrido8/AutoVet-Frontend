cc5faae50adabee281eb1dc68c9904fe93a8083e
garrido8 <adriangarrido15.9@gmail.com>
2025-05-20 16:27:21 +0200

    Ya se muestran las respuestas en el foro, falta pulir estética

--- src/app/general/components/forum-page/forum-page.component.ts
+++ src/app/general/components/forum-page/forum-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { AnswersService } from '../../../services/answers.service';
+import { Subscription } from 'rxjs';
+import { Answer } from '../../../interfaces/answer.interface';
 
 @Component({
   selector: 'app-forum-page',
@@ -6,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './forum-page.component.html',
   styleUrl: './forum-page.component.css'
 })
-export class ForumPageComponent {
+export class ForumPageComponent implements OnInit, OnDestroy {
+
+  private subscriptions = new Subscription();
+
+  private answesService = inject( AnswersService );
+
+  public allAnswers: Answer[] = [];
+  public keywords: string[] = [];
+
+  ngOnInit(): void {
+    this.answesService.getAnswers()
+      .subscribe( (data) => {
+        this.allAnswers = data;
+        this.allAnswers.forEach( (answer) => {
+          this.keywords.push( ...answer.keywords.split(',') );
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
 }
